95d7e7344d4cb896e7943371cca21e47011ee889
Christian Nikel <chnikel@gmail.com>
2023-04-30 15:39:42 +0200

    ✨ Add ability to drag elements into quickaccess

--- src/quickaccessView.ts
+++ src/quickaccessView.ts
@@ -7,6 +7,7 @@ export class QuickaccessView {
 
     const view = vscode.window.createTreeView("quickaccessView", {
       treeDataProvider: provider,
+      dragAndDropController: provider,
     });
     context.subscriptions.push(view);
 
@@ -20,7 +21,9 @@ export class QuickaccessView {
   }
 }
 
-class QuickaccessProvider implements vscode.TreeDataProvider<Item> {
+class QuickaccessProvider
+  implements vscode.TreeDataProvider<Item>, vscode.TreeDragAndDropController<Item>
+{
   constructor() {}
 
   items: Key[] = [
@@ -28,6 +31,47 @@ class QuickaccessProvider implements vscode.TreeDataProvider<Item> {
       path: "/home/christian/Dokumente/nuxt-vuetify-testing/.gitignore",
     },
   ];
+  dropMimeTypes: readonly string[] = ["text/uri-list"];
+  dragMimeTypes: readonly string[] = [];
+
+  private _onDidChangeTreeData: vscode.EventEmitter<Item[] | undefined | null | void> =
+    new vscode.EventEmitter<Item[] | undefined | null | void>();
+  // We want to use an array as the event type, but the API for this is currently being finalized. Until it's finalized, use any.
+  public onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+
+  handleDrop?(
+    target: Item | undefined,
+    dataTransfer: vscode.DataTransfer,
+    token: vscode.CancellationToken
+  ): void | Thenable<void> {
+    const transferItem = dataTransfer.get("text/uri-list");
+
+    if (!transferItem) {
+      return;
+    }
+
+    const resource = vscode.Uri.file(transferItem.value);
+    const filePath = resource.path.replace("/file://", "");
+
+    const itemExists = this.items.find((item) => item.path === filePath);
+    if (itemExists) {
+      return;
+    }
+
+    this.items.push({
+      path: filePath,
+    });
+
+    this._onDidChangeTreeData.fire();
+  }
+
+  resolveTreeItem?(
+    item: vscode.TreeItem,
+    element: Item,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.TreeItem> {
+    throw new Error("Method not implemented.");
+  }
 
   getTreeItem(element: Item): Item {
     return {
